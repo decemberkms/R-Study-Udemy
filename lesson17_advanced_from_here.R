0dfb1a755b9ffffaf0eb4fc95baf860d61e395a7
minsung_kim <decemberkms@gmail.com>
2020-07-09 14:32:21 +0200

    Add lessons

--- lesson17_advanced_from_here.R
+++ lesson17_advanced_from_here.R
@@ -4,7 +4,7 @@ df <- read.csv(file.choose())
 
 
 getwd()
-setwd("C:\\Users\\Min\\books\\R")
+setwd("C:\\Users\\Decemberkms.000\\books\\R\\R_Start")
 fin <- read.csv("P3-Future-500-The-Dataset.csv")
 
 str(fin)
@@ -72,7 +72,7 @@ fin[!complete.cases(fin),]
 fin[complete.cases(fin),]
 
 fin ################# na and just missing value is different!!!################
-fin <- read.csv("P3-Future-500-The-Dataset.csv", na.strings=c("")) ### howto fill missing char with NA!
+fin <- read.csv("P3-Future-500-The-Dataset.csv", na.strings=c("")) ### how to fill missing char with NA!
 
 
 ?complete.cases
@@ -120,6 +120,133 @@ fin[c(11,377),]
 
 fin[is.na(fin$State) & fin$City == "San Francisco", "State"] <- "CA"
 fin[c(82,265),]
+
+fin[complete.cases(fin),]
+fin[fin$Industry == 'Retail',]$Employees
+?median()
+
+
+# how to calculate mean or median excluding NA
+med_emp_retail <- median(fin[fin$Industry == "Retail","Employees"], na.rm=T)
+
+mean(fin[fin$Industry == "Retail","Employees"], na.rm=T)
+mean(fin[,"Employees"], na.rm=T)
+
+
+fin[is.na(fin$Employees) & fin$Industry == "Retail", "Employees"] <- med_emp_retail
+fin[3,]
+
+fin[!complete.cases(fin),]
+
+
+fin[is.na(fin$Employees), "Employees"]
+
+med_emp_fS <- median(fin[fin$Industry == "Financial Services", "Employees"], na.rm=T)
+fin[is.na(fin$Employees) & fin$Industry == "Financial Services", "Employees"] <- med_emp_fS
+mean(fin[fin$Industry == "Financial Services", "Employees"], na.rm=T)
+fin[332,]
+
+fin[!complete.cases(fin),]
+
+fin[is.na(fin$Growth) & fin$Industry == "Construction",]
+
+median(fin[, "Growth"], na.rm=T)
+med_grw_cost <- median(fin[fin$Industry == "Construction", "Growth"], na.rm=T)
+
+fin[is.na(fin$Growth), "Growth"] <- med_grw_cost
+fin[8,]
+
+
+fin[!complete.cases(fin),]
+
+fin[is.na(fin$Revenue),]  
+med_rev_construction <- median(fin[fin$Industry == "Construction", "Revenue"], na.rm=T)
+fin[is.na(fin$Revenue), "Revenue"] <- med_rev_construction
+fin[c(8,42),]
+
+
+fin[!complete.cases(fin),]
+
+
+med_exp_constr <- median(fin[fin$Industry == "Construction", 'Expenses'], na.rm=T)
+med_exp_constr
+fin[is.na(fin$Expenses) & fin$Industry == "Construction" & is.na(fin$Profit),'Expenses'] <- med_exp_constr
+
+fin[!complete.cases(fin),]
+
+
+fin[c(8,42),]
+
+# revenue - expenses = profit
+
+fin[is.na(fin$Profit),"Profit"] <- fin[is.na(fin$Profit), "Revenue"] - fin[is.na(fin$Profit), "Expenses"]
+
+
+
+
+fin[c(8,42),]
+
+
+fin[!complete.cases(fin$Expenses),"Expenses"] <- fin[is.na(fin$Expenses), "Revenue"] - fin[is.na(fin$Expenses), "Profit"]
+
+
+fin[15,]
+
+
+fin[!complete.cases(fin),]
+
+
+
+
+
+
+
+
+
+
+#---------------- visualisation
+install.packages("ggplot2")
+library(ggplot2)
+
+p <- ggplot(data=fin)
+
+p + geom_point(aes(x=Revenue, y=Expenses,
+                   color=Industry, size=Profit ))
+
+
+# smooth
+d <- ggplot(data=fin,aes(x=Revenue, y=Expenses, color=Industry))
+d + geom_point()
+d + geom_point() + geom_smooth(fill=NA, size=1.2)
+
+
+#------------- boxplot
+f <- ggplot(data=fin, aes(x=Industry, y=Growth,
+                          color=Industry))
+f + geom_boxplot(size=1)
+
+f + geom_jitter() + geom_boxplot(size=1, alpha=0.8, outlier.color = NA)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
